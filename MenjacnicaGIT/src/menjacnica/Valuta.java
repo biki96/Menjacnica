0e04847562e9105075ecc78d1d2082fc49f4d16b
Milos <bijanicmilos996@gmail.coom>
2017-03-29 00:30:19 +0200

    U klasi Valuta generisane su metode equals, toString i hashCode

--- MenjacnicaGIT/src/menjacnica/Valuta.java
+++ MenjacnicaGIT/src/menjacnica/Valuta.java
@@ -1,4 +1,4 @@
-package Menjacnica;
+package menjacnica;
 
 import java.util.GregorianCalendar;
 
@@ -45,6 +45,61 @@ public class Valuta {
 	public void setSrednji(double srednji) {
 		this.srednji = srednji;
 	}
+	@Override
+	public String toString() {
+		return "Valuta [nazivValute=" + nazivValute + ", skraceniNazivValute=" + skraceniNazivValute + ", datum="
+				+ datum + ", prodajni=" + prodajni + ", kupovni=" + kupovni + ", srednji=" + srednji + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((nazivValute == null) ? 0 : nazivValute.hashCode());
+		temp = Double.doubleToLongBits(prodajni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((skraceniNazivValute == null) ? 0 : skraceniNazivValute.hashCode());
+		temp = Double.doubleToLongBits(srednji);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(kupovni) != Double.doubleToLongBits(other.kupovni))
+			return false;
+		if (nazivValute == null) {
+			if (other.nazivValute != null)
+				return false;
+		} else if (!nazivValute.equals(other.nazivValute))
+			return false;
+		if (Double.doubleToLongBits(prodajni) != Double.doubleToLongBits(other.prodajni))
+			return false;
+		if (skraceniNazivValute == null) {
+			if (other.skraceniNazivValute != null)
+				return false;
+		} else if (!skraceniNazivValute.equals(other.skraceniNazivValute))
+			return false;
+		if (Double.doubleToLongBits(srednji) != Double.doubleToLongBits(other.srednji))
+			return false;
+		return true;
+	}
+	
+	
 	
 	
 	
