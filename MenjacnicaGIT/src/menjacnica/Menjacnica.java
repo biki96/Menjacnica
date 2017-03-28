284aedb252ba6327b38bfc7f7391e70607d31d43
Milos <bijanicmilos996@gmail.coom>
2017-03-29 01:10:14 +0200

    Implementirane su do kraja metode dodajKurs, izbrisiValutu i nadjiValutu.

--- MenjacnicaGIT/src/menjacnica/Menjacnica.java
+++ MenjacnicaGIT/src/menjacnica/Menjacnica.java
@@ -7,25 +7,49 @@ import interfejsMenjacnica.InterfejsMenjacnica;
 
 public class Menjacnica implements InterfejsMenjacnica {
 
-	
+	public LinkedList<Valuta> valute = new LinkedList<Valuta>();
 	
 	
 	@Override
 	public void dodajKurs(String nazivValute, String skraceniNazivValute, GregorianCalendar datum, double prodajni,
 			double kupovni, double srednji) {
-		// TODO Auto-generated method stub
-
+		Valuta v = new Valuta();
+		v.setDatum(datum);
+		v.setNazivValute(nazivValute);
+		v.setSkraceniNazivValute(skraceniNazivValute);
+		v.setKupovni(kupovni);
+		v.setProdajni(prodajni);
+		v.setSrednji(srednji);
+		if (v == null)
+			throw new RuntimeException("Nisu unete ispravno vrednosti!");
+		if (valute.contains(v))
+			throw new RuntimeException("Vec postoji!");
+		valute.add(v);
 	}
 
 	@Override
 	public void izbrisiValutu(String nazivValute, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+		if (nazivValute == null || datum == null){
+			throw new RuntimeException("Nisu unete ispravno vrednosti!");
+		}
+		for (int i = 0; i < valute.size(); i++){
+			if (valute.get(i).getNazivValute().equals(nazivValute) && valute.get(i).getDatum() == datum)
+				valute.remove(i);
+			break;
+		}
+		throw new RuntimeException("Data valuta ne postoji!");
 	}
 
 	@Override
 	public Valuta nadjiValutu(String nazivValute, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		if (nazivValute == null || datum == null){
+			throw new RuntimeException("Nisu unete ispravno vrednosti!");
+		}
+		for (int i = 0; i < valute.size(); i++){
+			if (valute.get(i).getNazivValute().equals(nazivValute) && valute.get(i).getDatum() == datum){
+				return valute.get(i);
+			}
+		}
 		return null;
 	}
 
