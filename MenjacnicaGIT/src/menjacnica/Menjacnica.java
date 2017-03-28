d964feabaff0705199760964f4d5de9f11c78eb4
Milos <bijanicmilos996@gmail.coom>
2017-03-29 00:58:58 +0200

    U paketu menjacnica napravljena je klasa "Menjacnica" koja implementira interfejs "InterfejsMenjacnica" sa svim metodama.

---
+++ MenjacnicaGIT/src/menjacnica/Menjacnica.java
@@ -0,0 +1,32 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
+import interfejsMenjacnica.InterfejsMenjacnica;
+
+public class Menjacnica implements InterfejsMenjacnica {
+
+	
+	
+	
+	@Override
+	public void dodajKurs(String nazivValute, String skraceniNazivValute, GregorianCalendar datum, double prodajni,
+			double kupovni, double srednji) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbrisiValutu(String nazivValute, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Valuta nadjiValutu(String nazivValute, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
